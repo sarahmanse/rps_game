c547e12ca4a5df2ae8a343313372e500b72afdd0
Sarahmanse <sarahmanse24@gmail.com>
2022-12-07 18:01:14 +0100

    update commit

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let playerScore = 0;
 let computerScore = 0;
 
 function game() {
+  playerScore = 0;
+  computerScore = 0;
   const rockBtn = document.querySelector('#rock');
   const paperBtn = document.querySelector('#paper');
   const scissorsBtn = document.querySelector('#scissors');
 
   const playerOptions = [rockBtn,paperBtn,scissorsBtn];
 
-  if(playerScore < 5 && computerScore < 5){
-    playerOptions.forEach(option => {
-      option.addEventListener('click', function() {
-        const options = ['rock','paper','scissors'];
-        const computerSelection = options[Math.floor(Math.random()*3)];
-        playRound(this.innerText, computerSelection);
-      })
+  playerOptions.forEach(option => {
+    option.addEventListener('click', function() {
+      const options = ['rock','paper','scissors'];
+      const computerSelection = options[Math.floor(Math.random()*3)];
+      playRound(this.innerText, computerSelection);
+      if (playerScore > 4 || computerScore > 4) {
+        gameOver(playerOptions);
+      }
     })
-  }
-  else {
-    gameOver();
-  }
-}  
+  })
+}
 
 function playRound(playerSelection, computerSelection) {
   const result = document.querySelector('.winner');
@@ -45,35 +45,30 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function gameOver() {
+function gameOver(playerOptions) {
   const chooseWeapon = document.querySelector('.chooseWeapon');
   const result = document.querySelector('.winner');
   const reloadBtn = document.querySelector('.reload');
 
   playerOptions.forEach(option => {
-    option.style.display = none;
+    option.style.display = 'none';
   })
 
   chooseWeapon.innerText = 'Game Over!';
 
   if(playerScore > computerScore){
-    result.style.fontSize = '2rem';
-    result.innerText = 'You Won The Game'
-    result.style.color = '#308D46';
+    result.style.fontSize = '4rem';
+    result.innerText = 'You won the game!!'
+    result.style.color = 'green';
   }
-  else if(playerScore < computerScore){
-      result.style.fontSize = '2rem';
-      result.innerText = 'You Lost The Game';
+  else {
+      result.style.fontSize = '4rem';
+      result.innerText = 'You lost the game :(';
       result.style.color = 'red';
-  }
-  else{
-      result.style.fontSize = '2rem';
-      result.innerText = 'Tie';
-      result.style.color = 'grey'
   }
 
   reloadBtn.innerText = 'Restart';
-  reloadBtn.style.display = 'flex'
+  reloadBtn.style.display = 'flex';
   reloadBtn.addEventListener('click',() => {
       window.location.reload();
   })
